b2fba85da3697620b34604ae9a1db611b5df6473
Himanshu Sharma <87695709+HimanshuS129@users.noreply.github.com>
2024-01-31 21:02:38 +0530

    init basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { Suspense, withTheme, lazy } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import styled from 'styled-components';
+import Loader from './components/Loader.tsx';
+
+const Home = lazy(() => import("./pages/Home.tsx"));
+
+const StyledAppContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--background-color-secondary);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledAppContainer>
+      <Suspense
+        fallback={
+          <Loader />
+        }
+      >
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </StyledAppContainer>
   );
 }
 
---
+++ src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MoonLoader } from 'react-spinners';
+
+const LoaderContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const Loader = () => {
+    return (
+        <LoaderContainer>
+            <MoonLoader color="#2026d2" size={60} aria-label="Loading spinner"/>
+        </LoaderContainer>
+    );
+};
+
+export default Loader;
+
+
